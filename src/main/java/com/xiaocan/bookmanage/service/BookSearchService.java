6f461283d1b3d05f1b29819fa5c943e59daddc6b
Git-xiaocan <940659468@qq.com>
2019-12-17 21:57:02 +0800

    增加图书搜索对象

--- src/main/java/com/xiaocan/bookmanage/service/BookSearchService.java
+++ src/main/java/com/xiaocan/bookmanage/service/BookSearchService.java
@@ -5,4 +5,10 @@ public class BookSearchService {
     private String ISBN;
     private String BookName;
     private String KeyWords;
+
+    public BookSearchService(String ISBN, String bookName, String keyWords) {
+        this.ISBN = ISBN;
+        BookName = bookName;
+        KeyWords = keyWords;
+    }
 }
